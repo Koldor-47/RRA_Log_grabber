87953d5e6d10b5ded544437eb9189755c5ae2c1b
Nick <nick.howlett@gmail.com>
2024-12-02 07:22:50 +0800

    Updating multi drills

--- Grabber.ps1
+++ Grabber.ps1
@@ -3,6 +3,7 @@
 
 # Surface Data Location
 $Surface_Manager_data_loc = "D:\ServerDataRoot_Backup\"
+$Test_Logs = "C:\Users\aucnh\Documents\Projects\D65 Autonomous\Logging\Surface Manager Data\ServerDataRoot_Backup"
 
 # Where the log file will save
 $time_Stamp = Get-Date -UFormat "%Y%m%d_%H%M%S"
@@ -12,6 +13,8 @@ $machines = @{
     'DR3132' = "8992013064"
     'DR3145' = "8992014939"
 }
+
+$machines_list = @("8992013064", "8992014939")
 
 $start_date = Read-Host "Enter a Start Date"
 $end_date = Read-Host "Enter a End date"
@@ -33,21 +36,32 @@ $current_date = $start_date
 
 Write-Host "Getting Machine Logs Now"
 
+foreach ($drill in $machines_list) {
+    $Archive_save_path = "$($script_loc)\DR3132_RRA_LOGS_$($time_Stamp).zip"
+    while($current_date -le $end_date){
+        try {
+            $year = Get-Date $current_date -Format yyyy
+            $month = Get-Date $current_date -Format MM
+            $day = Get-Date $current_date -Format dd
 
-$Archive_save_path = "$($script_loc)\DR3132_RRA_LOGS_$($time_Stamp).zip"
-while($current_date -le $end_date){
-    try {
-        $year = Get-Date $current_date -Format yyyy
-        $month = Get-Date $current_date -Format MM
-        $day = Get-Date $current_date -Format dd
-        $data_path = "$($Surface_Manager_data_loc)$($machines[$machine])\From Machine\prodout\RIGEVENT\$($year)-$($month)\$($day)"
-        Get-ChildItem -Path $data_path -ErrorAction Stop | Compress-Archive -update -DestinationPath $Archive_save_path
-        Write-Host "Found Logs $($machines[$machine]) on $($month)"
-        Write-Host $data_path
-    } catch [System.Management.Automation.ItemNotFoundException]{
-        Write-Host "Exception caught - No path"
+            # -----------===========| Using TEST Location |=================--------------------------------
+            
+            $data_path = "$($Test_Logs)\$($machines[$machine])\From Machine\prodout\RIGEVENT\$($year)-$($month)\$($day)"
+            
+            #
+            # Testing Path is correct
+            #
+            # Write-Host = $($data_path)
+            #
+            
+            Get-ChildItem -Path $data_path -ErrorAction Stop | Compress-Archive -update -DestinationPath $Archive_save_path
+            Write-Host "Found Logs $($machines[$machine]) on $($month)"
+            Write-Host $data_path
+        } catch [System.Management.Automation.ItemNotFoundException]{
+            Write-Host "Exception caught - No path"
+            }
+        
+        
+        $current_date = $current_date.AddDays(1)
         }
-    
-    
-    $current_date = $current_date.AddDays(1)
-}
+    }
